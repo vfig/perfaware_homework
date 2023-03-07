1e47b159b4f563433fb6fa7d38650264c6b94f9b
Andy Durdin <me@andy.durdin.net>
2023-03-07 19:31:33 +0000

    002 bonus homework

--- 002_decode/002_decode.c
+++ 002_decode/002_decode.c
@@ -56,14 +56,14 @@ enum regname {
 };
 
 static const char *reg_names[] = {
-    "AL", "BL", "CL", "DL",
-    "AH", "BH", "CH", "DH",
-    "AX", "BX", "CX", "DX",
-    "SP", "BP", "SI", "DI",
+    "al", "bl", "cl", "dl",
+    "ah", "bh", "ch", "dh",
+    "ax", "bx", "cx", "dx",
+    "sp", "bp", "si", "di",
 };
 
 enum argt {
-    argt_regname, argt_mem, argt_imm,
+    argt_regname, argt_regdisp, argt_imm, argt_mem,
 };
 
 struct insn_port {
@@ -71,17 +71,25 @@ struct insn_port {
     union {
         int regname;
         struct {
-            int mem_regname1;
-            int mem_regname2;
-            int mem_displacement;
-            int mem_use_displacement;
+            int regdisp_regname1;
+            int regdisp_regname2;
+            int16_t regdisp_displacement;
         };
         struct {
-            int imm_data;
+            uint16_t imm_data;
+        };
+        struct {
+            uint16_t mem_addr;
         };
     };
 };
 
+enum insn_port_select {
+    select_port_reg,
+    select_port_r_m,
+    select_port_imm,
+    select_port_mem,
+};
 
 struct insn_mov {
     int bits;
@@ -89,53 +97,58 @@ struct insn_mov {
     struct insn_port dst;
 };
 
-void fprint_mov_port(FILE *fout, struct insn_port port) {
+void fprint_mov_port(FILE *fout, struct insn_port port, int bits) {
     switch (port.argt) {
     case argt_regname:
         fputs(reg_names[port.regname], fout);
         break;
 
-    case argt_mem:
+    case argt_regdisp:
         fputs("[", fout);
-        if (port.mem_regname1!=NO_REGNAME) {
-            fputs(reg_names[port.mem_regname1], fout);
-            if (port.mem_regname2!=NO_REGNAME) {
+        if (port.regdisp_regname1==NO_REGNAME) {
+            fprintf(fout, "%d", port.regdisp_displacement);
+        } else {
+            fputs(reg_names[port.regdisp_regname1], fout);
+            if (port.regdisp_regname2!=NO_REGNAME) {
                 fputs("+", fout);
-                fputs(reg_names[port.mem_regname2], fout);
+                fputs(reg_names[port.regdisp_regname2], fout);
             }
-            if (port.mem_use_displacement && port.mem_displacement) {
-                fputs("+", fout);
-            }
-        }
-        if (port.mem_use_displacement) {
-            if (port.mem_displacement||port.mem_regname1==NO_REGNAME) {
-                fprintf(fout, "%d", port.mem_displacement);
+            if (port.regdisp_displacement) {
+                if (port.regdisp_displacement>0) {
+                    fprintf(fout, "+%d", port.regdisp_displacement);
+                } else if (port.regdisp_displacement<0) {
+                    fprintf(fout, "%d", port.regdisp_displacement);
+                }
             }
         }
         fputs("]", fout);
         break;
 
     case argt_imm:
+        switch (bits) {
+        case 8: fputs("byte ", fout); break;
+        case 16: fputs("word ", fout); break;
+        }
         fprintf(fout, "%d", port.imm_data);
+        break;
+
+    case argt_mem:
+        fputs("[", fout);
+        fprintf(fout, "%d", port.mem_addr);
+        fputs("]", fout);
         break;
 
     default:
         fprintf(stderr, "FUCKED UP\n");
-        fprintf(stderr, "  argt: %d\n", port.argt);
-        fprintf(stderr, "  regname: %d\n", port.regname);
-        fprintf(stderr, "  mem_regname1: %d\n", port.mem_regname1);
-        fprintf(stderr, "  mem_regname2: %d\n", port.mem_regname2);
-        fprintf(stderr, "  mem_displacement: %d\n", port.mem_displacement);
-        fprintf(stderr, "  mem_use_displacement: %d\n", port.mem_use_displacement);
-        exit(1);
+        abort();
     }
 }
 
 void fprint_mov_insn(FILE *fout, struct insn_mov mov) {
     fputs("mov ", fout);
-    fprint_mov_port(fout, mov.dst);
+    fprint_mov_port(fout, mov.dst, mov.bits);
     fputs(", ", fout);
-    fprint_mov_port(fout, mov.src);
+    fprint_mov_port(fout, mov.src, mov.bits);
     fputs("\n", fout);
 }
 
@@ -179,101 +192,204 @@ int effaddr_decode_displacement_bits(int r_m, int mod) {
 }
 
 void decode_stream(FILE *fin, FILE *fout, void (*error)(int where, char *fmt, ...) ) {
+    #define FAIL(fmt, ...) do{ error(ftell(fin), fmt, ##__VA_ARGS__); return; }while(0)
     #define READ_BYTE_OR_EOF(intvar) do{ intvar=fgetc(fin); }while(0)
-    #define READ_BYTE(intvar) do{ if ((intvar=fgetc(fin))==EOF) { error(ftell(fin), "EOF"); return; } }while(0)
+    #define READ_BYTE(intvar) do{ if ((intvar=fgetc(fin))==EOF) FAIL("unexpected eof"); }while(0)
     for (;;) {
+        int w;
+        int d;
+        int mod;
+        int reg;
+        int r_m;
+        int use_mod;
+        int use_reg;
+        int use_r_m;
+        int use_imm;
+        int use_mem;
+        int select_src;
+        int select_dst;
+
         int a;
         READ_BYTE_OR_EOF(a);
-        if (a==EOF) {
-            break;
-        }
-
+        if (a==EOF) break;
         switch (a) {
         // Register/memory to/from register
-        case 0x88: case 0x89: case 0x8a: case 0x8b: {
-            struct insn_mov mov;
-            int w = (a&0x01);
-            int d = (a&0x02)>>1;
-            mov.bits = w? 16 : 8;
-            READ_BYTE(a);
-            int mod = (a&0xc0)>>6;
-            int reg = (a&0x38)>>3;
-            int r_m = (a&0x07);
-            int memory_mode = (mod!=3);
-            struct insn_port port_reg;
-            port_reg.argt = argt_regname;
-            port_reg.regname = regname_decode(reg, w);
-            struct insn_port port_r_m;
-            if (memory_mode) {
-                port_r_m.argt = argt_mem;
-                port_r_m.mem_regname1 = effaddr_decode_regname1(r_m);
-                port_r_m.mem_regname2 = effaddr_decode_regname2(r_m);
-                int displacement_bytes = effaddr_decode_displacement_bits(r_m, mod)/8;
-                port_r_m.mem_use_displacement = (displacement_bytes!=0);
-                int displacement = 0;
-                if (displacement_bytes) {
-                    int b;
-                    READ_BYTE(b);
-                    displacement |= b;
-                    --displacement_bytes;
-                }
-                if (displacement_bytes) {
-                    int b;
-                    READ_BYTE(b);
-                    displacement |= (b<<8);
-                    --displacement_bytes;
-                }
-                port_r_m.mem_displacement = displacement;
-            } else {
-                port_r_m.argt = argt_regname;
-                port_r_m.regname = regname_decode(r_m, w);
-            }
+        case 0x88: case 0x89: case 0x8a: case 0x8b:
+            w = (a&0x01);
+            d = (a&0x02)>>1;
+            use_mod = 1;
+            use_reg = 1;
+            use_r_m = 1;
+            use_imm = 0;
+            use_mem = 0;
             if (d) {
-                mov.src = port_r_m;
-                mov.dst = port_reg;
+                select_src = select_port_r_m;
+                select_dst = select_port_reg;
             } else {
-                mov.src = port_reg;
-                mov.dst = port_r_m;
+                select_src = select_port_reg;
+                select_dst = select_port_r_m;
             }
-            fprint_mov_insn(fout, mov);
-        } break;
+            goto decode_mov;
+
+        // Immediate to register/memory
+        case 0xc6: case 0xc7:
+            w = (a&0x01);
+            use_mod = 1;
+            use_reg = 0;
+            use_r_m = 1;
+            use_imm = 1;
+            use_mem = 0;
+            select_src = select_port_imm;
+            select_dst = select_port_r_m;
+            goto decode_mov;
 
         // Immediate to register
         case 0xb0: case 0xb1: case 0xb2: case 0xb3:
         case 0xb4: case 0xb5: case 0xb6: case 0xb7:
         case 0xb8: case 0xb9: case 0xba: case 0xbb:
-        case 0xbc: case 0xbd: case 0xbe: case 0xbf: {
+        case 0xbc: case 0xbd: case 0xbe: case 0xbf:
+            w = (a&0x08);
+            reg = (a&0x03);
+            use_mod = 0;
+            use_reg = 1;
+            use_r_m = 0;
+            use_imm = 1;
+            use_mem = 0;
+            select_src = select_port_imm;
+            select_dst = select_port_reg;
+            goto decode_mov;
+
+        // Memory to accumulator
+        case 0xa0: case 0xa1:
+            w = (a&0x01);
+            reg = 0;
+            use_mod = 0;
+            use_reg = 1;
+            use_r_m = 0;
+            use_imm = 0;
+            use_mem = 1;
+            select_src = select_port_mem;
+            select_dst = select_port_reg;
+            goto decode_mov;
+
+        // Accumulator to memory
+        case 0xa2: case 0xa3:
+            w = (a&0x01);
+            reg = 0;
+            use_mod = 0;
+            use_reg = 1;
+            use_r_m = 0;
+            use_imm = 0;
+            use_mem = 1;
+            select_src = select_port_reg;
+            select_dst = select_port_mem;
+            goto decode_mov;
+
+        // ---- MOV decode ----
+        decode_mov: {
             struct insn_mov mov;
-            int w = (a&0x08);
-            int reg = (a&0x03);
             mov.bits = w? 16 : 8;
+            if (use_mod) {
+                READ_BYTE(a);
+                mod = (a&0xc0)>>6;
+                reg = (a&0x38)>>3;
+                r_m = (a&0x07);
+            }
+            // DIRECT ADDRESS MODE: detour via use_mem instead of use_r_m:
+            if (use_mod && use_r_m && r_m==6 && mod==0) {
+                use_r_m = 0;
+                use_mem = 1;
+                if (select_src==select_port_r_m) select_src = select_port_mem;
+                if (select_dst==select_port_r_m) select_dst = select_port_mem;
+            }
             struct insn_port port_reg;
-            port_reg.argt = argt_regname;
-            port_reg.regname = regname_decode(reg, w);
-            struct insn_port port_data;
-            port_data.argt = argt_imm;
-            int data_bytes = mov.bits/8;
-            int data = 0;
-            if (data_bytes) {
+            struct insn_port port_r_m;
+            struct insn_port port_imm;
+            struct insn_port port_mem;
+            if (use_reg) {
+                port_reg.argt = argt_regname;
+                port_reg.regname = regname_decode(reg, w);
+            }
+            if (use_r_m) {
+                int memory_mode = (mod!=3);
+                if (memory_mode) {
+                    port_r_m.argt = argt_regdisp;
+                    port_r_m.regdisp_regname1 = effaddr_decode_regname1(r_m);
+                    port_r_m.regdisp_regname2 = effaddr_decode_regname2(r_m);
+                    int displacement_bytes = effaddr_decode_displacement_bits(r_m, mod)/8;
+                    int16_t displacement = 0;
+                    if (displacement_bytes) {
+                        // Low 8 bits
+                        int b;
+                        READ_BYTE(b);
+                        displacement |= b;
+                        --displacement_bytes;
+                    }
+                    if (displacement_bytes) {
+                        // High 8 bits
+                        int b;
+                        READ_BYTE(b);
+                        displacement |= (b<<8);
+                        --displacement_bytes;
+                    } else {
+                        // Sign-extend 8-bit displacement
+                        if (displacement&0x80)
+                            displacement |= 0xFF00;
+                    }
+                    port_r_m.regdisp_displacement = displacement;
+                } else {
+                    port_r_m.argt = argt_regname;
+                    port_r_m.regname = regname_decode(r_m, w);
+                }
+            }
+            if (use_mem) {
+                port_mem.argt = argt_mem;
+                uint16_t addr = 0;
                 int b;
                 READ_BYTE(b);
-                data |= b;
-                --data_bytes;
-            }
-            if (data_bytes) {
-                int b;
+                addr |= b;
                 READ_BYTE(b);
-                data |= (b<<8);
-                --data_bytes;
+                addr |= (b<<8);
+                port_mem.mem_addr = addr;
             }
-            port_data.imm_data = data;
-            mov.src = port_data;
-            mov.dst = port_reg;
+            if (use_imm) {
+                port_imm.argt = argt_imm;
+                int data_bytes = w?2:1;
+                uint16_t data = 0;
+                if (data_bytes) {
+                    int b;
+                    READ_BYTE(b);
+                    data |= b;
+                    --data_bytes;
+                }
+                if (data_bytes) {
+                    int b;
+                    READ_BYTE(b);
+                    data |= (b<<8);
+                    --data_bytes;
+                }
+                port_imm.imm_data = data;
+            }
+            switch (select_src) {
+            case select_port_reg: mov.src = port_reg; break;
+            case select_port_r_m: mov.src = port_r_m; break;
+            case select_port_imm: mov.src = port_imm; break;
+            case select_port_mem: mov.src = port_mem; break;
+            default:
+                FAIL("select src port failure.");
+            }
+            switch (select_dst) {
+            case select_port_reg: mov.dst = port_reg; break;
+            case select_port_r_m: mov.dst = port_r_m; break;
+            case select_port_mem: mov.dst = port_mem; break;
+            default:
+                FAIL("select dst port failure.");
+            }
             fprint_mov_insn(fout, mov);
         } break;
 
         default:
-            error(ftell(fin), "Unknown opcode 0x%02x", a);
+            FAIL("Unknown opcode 0x%02x", a);
             return;
 
         } /* end switch */
@@ -287,7 +403,7 @@ void fatal_error(int where, char *fmt, ...) {
     vfprintf(stderr, fmt, args);
     va_end(args);
     fprintf(stderr, "\n");
-    exit(1);
+    abort();
 }
 
 int main(int argc, char **argv) {
