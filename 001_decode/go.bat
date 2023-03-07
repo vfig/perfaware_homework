4b5c63813a9abda920021a7558b9376413be000c
Andy Durdin <me@andy.durdin.net>
2023-03-07 00:45:11 +0000

    001_decode: fix path to nasm

--- 001_decode/go.bat
+++ 001_decode/go.bat
@@ -1,18 +1,19 @@
 @echo off
 set DIFF="C:\Program Files\Git\usr\bin\diff.exe" --binary -q
+set NASM="..\nasm.exe"
 
 cl 001_decode.c
 if errorlevel 1 goto fail
 
 set fname=listing_0037_single_register_mov
 001_decode < %fname% > %fname%_out.asm
-nasm %fname%_out.asm
+%NASM% %fname%_out.asm
 %DIFF% %fname% %fname%_out
 if errorlevel 1 goto fail
 
 set fname=listing_0038_many_register_mov
 001_decode < %fname% > %fname%_out.asm
-nasm %fname%_out.asm
+%NASM% %fname%_out.asm
 %DIFF% %fname% %fname%_out
 if errorlevel 1 goto fail
 
