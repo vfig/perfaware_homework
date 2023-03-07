7ee67d70fb82c47afeaf9ed6706530841355099b
Andy Durdin <me@andy.durdin.net>
2023-03-07 18:54:17 +0000

    reorganise listings and simplify go.bat

--- 001_decode/go.bat
+++ 001_decode/go.bat
@@ -1,21 +1,22 @@
 @echo off
 set DIFF="C:\Program Files\Git\usr\bin\diff.exe" --binary -q
 set NASM="..\nasm.exe"
+set LISTINGS=..\listings
+set name=001_decode
 
-cl 001_decode.c
+cl /nologo /Zi %name%.c
 if errorlevel 1 goto fail
 
-set fname=listing_0037_single_register_mov
-001_decode < %fname% > %fname%_out.asm
-%NASM% %fname%_out.asm
-%DIFF% %fname% %fname%_out
-if errorlevel 1 goto fail
-
-set fname=listing_0038_many_register_mov
-001_decode < %fname% > %fname%_out.asm
-%NASM% %fname%_out.asm
-%DIFF% %fname% %fname%_out
-if errorlevel 1 goto fail
+for %%f in (
+    listing_0037_single_register_mov
+    listing_0038_many_register_mov
+) do (
+    set fname=%%f
+    %name%.exe < %LISTINGS%\%fname% > %fname%_out.asm
+    %NASM% %fname%_out.asm
+    %DIFF% %LISTINGS%\%fname% %fname%_out
+    if errorlevel 1 goto fail
+)
 
 goto end
 :fail
--- 002_decode/go.bat
+++ 002_decode/go.bat
@@ -1,21 +1,24 @@
 @echo off
 set DIFF="C:\Program Files\Git\usr\bin\diff.exe" --binary -q
 set NASM="..\nasm.exe"
+set LISTINGS=..\listings
+set name=002_decode
 
-cl /Zi 002_decode.c
+cl /nologo /Zi %name%.c
 if errorlevel 1 goto fail
 
-set fname=listing_0039_more_mov
-002_decode < %fname% > %fname%_out.asm
-%NASM% %fname%_out.asm
-%DIFF% %fname% %fname%_out
-if errorlevel 1 goto fail
-
-set fname=listing_0040_even_more_mov
-002_decode < %fname% > %fname%_out.asm
-%NASM% %fname%_out.asm
-%DIFF% %fname% %fname%_out
-if errorlevel 1 goto fail
+for %%f in (
+    listing_0037_single_register_mov
+    listing_0038_many_register_mov
+    listing_0039_more_mov
+    listing_0040_even_more_mov
+) do (
+    set fname=%%f
+    %name%.exe < %LISTINGS%\%fname% > %fname%_out.asm
+    %NASM% %fname%_out.asm
+    %DIFF% %LISTINGS%\%fname% %fname%_out
+    if errorlevel 1 goto fail
+)
 
 goto end
 :fail
